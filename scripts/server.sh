d7b917c452066d2f0349e363e97858ae216faed9
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2023-05-14 19:21:42 +0200

    chore: specify hugo version in scripts and gh action (#2)

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $(pwd):/src -p 1313:1313 -u 1000:1000 klakegg/hugo:ext server -D
+docker run --rm -it -v $(pwd):/src -p 1313:1313 -u 1000:1000 -e HUGO_VERSION_OVERRIDE=0.111.3 klakegg/hugo:ext server -D
